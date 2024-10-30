94049cb084b909f693ba977467a2fdd3e096a1d4
LAPTOP-NIOUD1BM\pc <huyt24309@gmail.com>
2024-10-30 17:53:48 +0700

    no message

--- Core/Src/input_reading.c
+++ Core/Src/input_reading.c
@@ -13,6 +13,8 @@ int buttonCounter_500ms = 0;
 int buttonBuffer[NUM_OF_BUTTONS];
 int flag_reset = 0;
 int flag_set = 0;
+int flag_change = 0;
+int flag_holding = 0;
 int value = 0;
 int mode = 0;
 
@@ -21,8 +23,10 @@ void readButton(void) {
     for (int i = 0; i < NUM_OF_BUTTONS; i++) {
         lastButton[i] = thisButton[i];
         thisButton[i] = HAL_GPIO_ReadPin(portBuffer[i], pinBuffer[i]);
-
-        if (thisButton[i] == lastButton[i]) {
+        if ((thisButton[i] == lastButton[i]) && (thisButton[i] == lastButton[i - 1])) {
+            if (lastButton[i - 1] != lastButton[i - 2]) {
+                lastButton[i - 2] = lastButton[i - 1];
+            }
             switch (buttonBuffer[i]) {
                 case RELEASED:
                     buttonBuffer[i] = thisButton[i];
@@ -30,13 +34,13 @@ void readButton(void) {
                 case PRESSED:
                     if (thisButton[i] == PRESSED) {
                         if (i == 0 && buttonCounter[0] == 0) {
-                            flag_reset = 1;
+                            flag_reset = 1;//
                             value = 0;
                             mode++;
-                        } else if (i == 1 && buttonCounter[1] == 0) {
+                        } else if (i == 1 && buttonCounter[1] == 0) {//
                             value++;
                             if (value > 99) value = 0;
-                        } else if (i == 2 && buttonCounter[2] == 0) flag_set = 1;
+                        } else if (i == 2 && buttonCounter[2] == 0) flag_set = 1;//
 
                         if (buttonCounter[i] <= HOLDED) {
                         	buttonCounter[i]++;
@@ -72,6 +76,9 @@ void readButton(void) {
     }
 }
 
+//void setvalue(){s
+//
+//}
 int getButton(int index){
 	if(index >= NUM_OF_BUTTONS) return -1;
 	else return buttonBuffer[index];
