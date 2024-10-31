7db39a033f582e3b166aac260fc5479c5502c02e
LAPTOP-NIOUD1BM\pc <huyt24309@gmail.com>
2024-10-31 22:45:19 +0700

    no message

--- Core/Src/input_reading.c
+++ Core/Src/input_reading.c
@@ -15,95 +15,108 @@ int flag_change = 0;
 int flag_holding = 0;
 int value = 0;
 int mode = 0;
-int KeyReg0[NUM_OF_BUTTONS] = {0};
-int KeyReg1[NUM_OF_BUTTONS] = {0};
-int KeyReg2[NUM_OF_BUTTONS] = {0};
-int lastStableButtonState[NUM_OF_BUTTONS] = {0};
+#define DEBOUNCE_THRESHOLD 3
+uint8_t debounceCounter[NUM_OF_BUTTONS] = {0};
 
 void readButton(void) {
     for (int i = 0; i < NUM_OF_BUTTONS; i++) {
-        lastButton[i] = thisButton[i];
-        thisButton[i] = HAL_GPIO_ReadPin(portBuffer[i], pinBuffer[i]);
-        KeyReg2[i] = KeyReg1[i];
-        KeyReg1[i] = KeyReg0[i];
-        KeyReg0[i] = thisButton[i];
+        uint8_t currentState = HAL_GPIO_ReadPin(portBuffer[i], pinBuffer[i]);
+        if (currentState == thisButton[i]) {
+            if (debounceCounter[i] < DEBOUNCE_THRESHOLD) {
+                debounceCounter[i]++;
+            } else {
+                lastButton[i] = thisButton[i];
+                switch (buttonBuffer[i]) {
+                    case RELEASED:
+                        buttonBuffer[i] = currentState;
+                        break;
 
-        if ((KeyReg1[i] == KeyReg0[i]) && (KeyReg1[i] == KeyReg2[i])) {
-            if (KeyReg2[i] != lastStableButtonState[i]) {
-                lastStableButtonState[i] = KeyReg2[i];
-                buttonBuffer[i] = (lastStableButtonState[i] == PRESSED) ? PRESSED : RELEASED;
+                    case PRESSED:
+                        if (currentState == PRESSED) {
+                            if (i == 0 && buttonCounter[0] == 0) {
+                                flag_reset = 1;
+                                value = 0;
+                                mode++;
+                            } else if (i == 1 && buttonCounter[1] == 0) {
+                                value++;
+                                if (value > 99) value = 0;
+                            } else if (i == 2 && buttonCounter[2] == 0) {
+                                flag_set = 1;
+                            }
+
+                            if (buttonCounter[i] <= HOLDED) {
+                                buttonCounter[i]++;
+                            } else {
+                                buttonCounter[i] = 0;
+                                buttonBuffer[i] = INCREASE;
+                                break;
+                            }
+                        } else {
+                            buttonCounter[i] = 0;
+                            buttonBuffer[i] = RELEASED;
+                        }
+                        break;
+
+                    case INCREASE:
+                        if (currentState == PRESSED) {
+                            if (buttonCounter[i] % 10 == 0) {
+                                value++;
+                                if (value > 99) value = 0;
+                            }
+                            buttonCounter[i]++;
+                        } else {
+                            buttonCounter[i] = 0;
+                            buttonBuffer[i] = RELEASED;
+                        }
+                        break;
+
+                    default:
+                        break;
+                }
             }
-        }
-
-        switch (buttonBuffer[i]) {
-            case RELEASED:
-                buttonBuffer[i] = thisButton[i];
-                break;
-            case PRESSED:
-                if (thisButton[i] == PRESSED) {
-                    setFlagsAndHandleActions(i);
-
-                    if (buttonCounter[i] <= HOLDED) {
-                        buttonCounter[i]++;
-                    } else {
-                        buttonCounter[i] = 0;
-                        buttonBuffer[i] = INCREASE;
-                    }
-                } else {
-                    buttonCounter[i] = 0;
-                    buttonBuffer[i] = RELEASED;
-                }
-                break;
-            case INCREASE:
-                if (thisButton[i] == PRESSED) {
-                    handleIncrease(i);
-                } else {
-                    buttonCounter[i] = 0;
-                    buttonBuffer[i] = RELEASED;
-                }
-                break;
-            default:
-                break;
-        }
-    }
-}
-
-void setFlagsAndHandleActions(int i) {
-    if (i == 0 && buttonCounter[0] == 0) {
-        flag_reset = 1;
-        value = 0;
-        mode++;
-    } else if (i == 1 && buttonCounter[1] == 0) {
-        value++;
-        if (value > 99) value = 0;
-    } else if (i == 2 && buttonCounter[2] == 0) {
-        flag_set = 1;
-    }
-    if(mode != 1) {
-            HAL_GPIO_WritePin(GPIOB, GPIO_PIN_14, SET);
         } else {
-            HAL_GPIO_WritePin(GPIOB, GPIO_PIN_14, RESET);
+            debounceCounter[i] = 0;
+            thisButton[i] = currentState;
         }
-
-        if (mode > 4) {
-            mode = 1;
-            setTime();
-        }
-
-        if (flag_reset) {
-            resetPin();
-            flag_reset = 0;
-            value = 0;
-        }
-}
-
-void handleIncrease(int i) {
-    if (buttonCounter[i] % 10 == 0) {
-        value++;
-        if (value > 99) value = 0;
     }
-    buttonCounter[i]++;
 }
+
+//void setFlagsAndHandleActions(int i) {
+//    if (i == 0 && buttonCounter[0] == 0) {
+//        flag_reset = 1;
+//        value = 0;
+//        mode++;
+//    } else if (i == 1 && buttonCounter[1] == 0) {
+//        value++;
+//        if (value > 99) value = 0;
+//    } else if (i == 2 && buttonCounter[2] == 0) {
+//        flag_set = 1;
+//    }
+//    if(mode != 1) {
+//            HAL_GPIO_WritePin(GPIOB, GPIO_PIN_14, SET);
+//        } else {
+//            HAL_GPIO_WritePin(GPIOB, GPIO_PIN_14, RESET);
+//        }
+//
+//        if (mode > 4) {
+//            mode = 1;
+//            setTime();
+//        }
+//
+//        if (flag_reset) {
+//            resetPin();
+//            flag_reset = 0;
+//            value = 0;
+//        }
+//}
+//
+//void handleIncrease(int i) {
+//    if (buttonCounter[i] % 10 == 0) {
+//        value++;
+//        if (value > 99) value = 0;
+//    }
+//    buttonCounter[i]++;
+//}
 
 int getButton(int index){
 	if(index >= NUM_OF_BUTTONS) return -1;
